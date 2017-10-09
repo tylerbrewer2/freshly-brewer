40d19dfb4c2612b5ea33a9d4e8f85042b706cde9
tylerbrewer2 <tbrewer1@mail.mvnu.edu>
2017-10-08 22:02:04 -0400

    photo cred

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,6 +5,7 @@ import Col from '../Col';
 import Row from '../Row';
 import Section from '../Section';
 import FloatWrapper from '../FloatWrapper';
+import BodyText from '../BodyText';
 import FooterLink from './FooterLink';
 
 // Style Helpers
@@ -16,7 +17,9 @@ const FooterCore = ({ className }) => {
     <footer className={className}>
       <Row middle>
         <Col  mobileWidth={12}>
-          417 Benninghaus Rd, Baltimore, MD 21212
+          <BodyText>
+            All photos taken by <a href="https://www.instagram.com/mikaylaherrick/">Mikayla Herrick</a>.
+          </BodyText>
         </Col>
       </Row>
     </footer>
@@ -24,15 +27,31 @@ const FooterCore = ({ className }) => {
 }
 
 const Footer = styled(FooterCore)`
-  font-size: 12px;
   padding: 40px;
   margin: 0 auto;
   margin-top: 40px;
   border-top: 1px solid ${Colors.richBlack};
 
+  div {
+    text-align: right;
+  }
+
+  a {
+    color: ${Colors.richBlack};
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
   @media (${MediaWidths.mobile}) {
     margin-left: 0;
     margin-top: 20px;
+
+    div {
+      text-align: left;
+    }
   }
 `
 
